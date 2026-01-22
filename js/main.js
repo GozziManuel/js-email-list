9ebffafa2fa85b48a2cade39b70cf5139ccdf5c5
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-22 14:35:10 +0100

    10 email cycle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log(axios);
+for (let i = 0; i < 10; i++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((resp) => {
+      const email = resp.data.response;
+      console.log(email);
+    });
+}
